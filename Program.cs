48182e819608a66540bcc80f490f6fb1a526f15e
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-05 17:59:55 -0500

    Initial commit of c2f project scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c2f
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
