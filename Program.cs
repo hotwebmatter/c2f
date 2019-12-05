0f57361e153a6d4757c1107a3efa568079184f03
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-05 18:39:15 -0500

    Remove superfluous parentheses

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace c2f
         static void Main(string[] args)
         {
             double tempCelsius = 0;
-            double tempFahrenheit = ((tempCelsius * 9) / 5) + 32;
+            double tempFahrenheit = (tempCelsius * 9 / 5) + 32;
             WriteLine("{0, 8}{1, 12}", "Celsius", "Fahrenheit");
             WriteLine("{0, 8:F0}{1, 12:F1}", tempCelsius, tempFahrenheit);
             ReadLine();
