86717c455d1e1f29ea07bf36bd36178b9ec59189
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-05 18:30:25 -0500

    Change compile-time initialization to zero, per spec

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace c2f
     {
         static void Main(string[] args)
         {
-            double tempCelsius = 32;
+            double tempCelsius = 0;
             double tempFahrenheit = ((tempCelsius * 9) / 5) + 32;
             WriteLine("{0, 12}{1, 12}", "Celsius", "Fahrenheit");
             WriteLine("{0, 12:F0}{1, 12:F1}", tempCelsius, tempFahrenheit);
