e2157231e2ef9f71b0b6b60e241da1f89fc0f3e3
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-05 18:28:56 -0500

    Display values number-aligned

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace c2f
         {
             double tempCelsius = 32;
             double tempFahrenheit = ((tempCelsius * 9) / 5) + 32;
+            WriteLine("{0, 12}{1, 12}", "Celsius", "Fahrenheit");
+            WriteLine("{0, 12:F0}{1, 12:F1}", tempCelsius, tempFahrenheit);
             ReadLine();
         }
     }
