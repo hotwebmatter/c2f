ecbfa772a92e0785fb38c0bffe047a8424d90f80
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-05 18:20:24 -0500

    Use static System.Console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace c2f
 {
@@ -6,7 +7,7 @@ namespace c2f
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReadLine();
         }
     }
 }
