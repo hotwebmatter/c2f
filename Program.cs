5f8022490cf24fb0ec9511ec6d7b92f8975ad360
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-06 18:46:55 -0500

    Add example from Kernighan & Ritchie

--- Program.cs
+++ Program.cs
@@ -24,6 +24,27 @@ namespace c2f
                 tempFahrenheit = (tempCelsius * 9 / 5) + 32;
                 WriteLine("{0, 8:F0}{1, 12:F1}", tempCelsius, tempFahrenheit);
             }
+            WriteLine("\n");
+            /**
+             * BONUS: Fahrenheit to Celsius table
+             *
+             * Based on example from "The C Programming Language"
+             * by Brian W. Kernighan and Dennis Ritchie (1978)
+             */
+
+            int fahr, celsius;
+            const int LOWER = 0;     // lower limit of temperature scale
+            const int UPPER = 300;   // upper limit
+            const int STEP = 20;     // step size
+
+            WriteLine("{0, 12}{1, 8}", "Fahrenheit", "Celsius");
+            fahr = LOWER;
+            while (fahr <= UPPER)
+            {
+                celsius = 5 * (fahr - 32) / 9;
+                WriteLine("{0, 12}{1, 8}", fahr, celsius);
+                fahr = fahr + STEP;
+            }
 
             ReadLine();
         }
